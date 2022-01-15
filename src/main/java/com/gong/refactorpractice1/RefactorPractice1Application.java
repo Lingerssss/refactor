eb2b5c3c1b1ab90d1450128fe5efff043e26fb29
Jiaxi Gong <gongjiaxi@126.com>
2022-01-16 02:04:17 +0800

    add: employeeServiceTest

---
+++ src/main/java/com/gong/refactorpractice1/RefactorPractice1Application.java
@@ -0,0 +1,13 @@
+package com.gong.refactorpractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactorPractice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gong/refactorpractice1/RefactorPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gong.refactorpractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

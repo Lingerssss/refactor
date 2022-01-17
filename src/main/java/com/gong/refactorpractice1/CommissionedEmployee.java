31c54e72d81d1b5bb19181e34b5ce4abab2d4b25
Yan Qian <qianyan@thoughtworks.com>
2022-01-17 17:09:59 +0800

    refactor(Employee): copy the calculateCommissionedPay to CommissionedEmployee

--- src/main/java/com/gong/refactorpractice1/CommissionedEmployee.java
+++ src/main/java/com/gong/refactorpractice1/CommissionedEmployee.java
@@ -6,6 +6,6 @@ import com.gong.refactorpractice1.entity.Payment;
 public class CommissionedEmployee extends Employee {
     @Override
     public Payment calculatePay() {
-        return null;
+        return new Payment(1);
     }
 }
